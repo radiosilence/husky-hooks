c553edc3a9997288d5312e388369127e6e49b5e8
James Cleveland <jc@blit.cc>
2018-04-05 11:36:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+husky-hooks
+===========
+
+Some handly command line utils to use in git hooks
+
+## commit-msg
+
+### add-ticket-prefix
+
+`add-ticket-prefix ${GIT_PARAMS}`
+
---
+++ src/add-ticket-prefix.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs'
+const branch = require('git-branch')
+
+// Get the filename for commit message
+const name = process.argv[2]
+
+// Take existing message in file and strip out empty/comments
+const existing = fs
+    .readFileSync(name, 'utf-8')
+    .split('\n')
+    .filter((line) => !line.trimLeft().startsWith('#'))
+    .filter(Boolean)
+
+if (existing.length === 0) {
+    console.error('Aborting commit due to empty commit message.')
+    process.exit(-1)
+}
+
+// Strip the words from the ticket
+const branchName = branch.sync()
+
+let prefix = ''
+
+if (
+    typeof branchName === 'string' &&
+    branchName.match(/^[A-Z]{3}-[0-9]{1,4}/)
+) {
+    const ticket = branchName.split('_')[0]
+    prefix = `[${ticket}] `
+}
+
+// Prepend the shortened branch name
+fs.writeFileSync(name, `${prefix}${existing.join('\n')}`)
