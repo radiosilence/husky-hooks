96fc41a06694628651f084dd2865a0fe465429d4
James Cleveland <james.cleveland@zopa.com>
2018-11-21 14:08:08 +0000

    use regex

--- add-ticket-prefix.sh
+++ add-ticket-prefix.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
 existing=$(grep -o '^[^#]*' $1)
-branch=$(git rev-parse --abbrev-ref HEAD)
-IFS='_' read -ra ticket <<< "$branch"
 if [ $(grep -o '^[^#]*' $1 | wc -l) = "0" ]; then
   echo 'Aborting commit due to empty commit message.'
   exit -1
-fi
+branch=$(git rev-parse --abbrev-ref HEAD)
+ticket=$(echo $branch | perl -lne '/([A-Z]+-\d+)/ && print $1')
 prefix=""
 if [ $branch != "master" ]; then
   prefix="[$ticket] "
